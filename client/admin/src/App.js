27f7334dc8e440164b999995f0fd4f5423f6ab65
HoshiyamaSeizen <nazarafanasyev@gmail.com>
2022-12-01 02:33:06 +0300

    redux

--- client/admin/src/App.js
+++ client/admin/src/App.js
@@ -1,14 +1,19 @@
 import { RouterProvider } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
 import { Router } from './Router';
+import { StocksStore } from './store/Store';
 import { Theme } from './Theme';
 
 import './styles/App.sass';
 
 function App() {
 	return (
-		<Theme>
-			<RouterProvider router={Router} />
-		</Theme>
+		<Provider store={StocksStore}>
+			<Theme>
+				<RouterProvider router={Router} />
+			</Theme>
+		</Provider>
 	);
 }
 
